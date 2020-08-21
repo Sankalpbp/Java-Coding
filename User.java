36c30ef6dcbafcd9e85ce4be73d9a27bde9e089b
Sankalp Arora <sankalpnitj@gmail.com>
2020-08-21 21:30:55 +0530

    Initial Commit

---
+++ User.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class User {
+		
+	public String name;
+
+	public User (String name) {
+		this.name = name;
+	}
+	
+	public void greet () {
+		System.out.println("Welcome to the Java World, " + this.name + "\n");
+	}
+
+	public static void main (String [] args) {
+			
+		System.out.println("What is your name?");
+		Scanner scanner = new Scanner (System.in);
+
+		String name = scanner.next();
+
+		User user1 = new User (name);
+
+		user1.greet();
+	}
+}
